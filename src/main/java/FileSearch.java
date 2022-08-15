0a1ff91b7ca5e6922fab4d15f34dad9e5e0ee0ee
TarasovE_NOTEBOOKASUS <trsv.egor@gmail.com>
2022-08-15 20:53:14 +0300

    step2

--- src/main/java/FileSearch.java
+++ src/main/java/FileSearch.java
@@ -1,38 +1,99 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class FileSearch  {
     public static void searcher(String request, int column) throws FileNotFoundException {
-
-        File file = new File("src/main/resources/airports.dat");
-
-        Scanner scanner = new Scanner(file);
-
+        column--;
         long strt = System.currentTimeMillis();
+
+//-------------------------------------------------------------------------------------------------
+    //TreeSet hassh
+        BufferedReader reader;
+
         int lines = 0;
+        try {
+            reader = new BufferedReader(new FileReader("src\\airports.dat"));
+            String file;
+            while ((file = reader.readLine()) != null) {
+                String[] columns = file.split(",");
 
-        //ВЫбрать лучший способ поиска
-        while (scanner.hasNextLine()){
-            String line = scanner.nextLine();
-            String[] columns = line.split(",");
-
-
-
-            //Исправить как он ищет
-            if (columns[column].contains(request)){
+                if (columns[column].indexOf(request) == 1){
                 lines++;
-                System.out.println( columns[column] + "[" + line + "]");
+                System.out.println( columns[column] + "[" + file + "]");
 
             }
 
+
+//                Collections.sort(rows);
+//                int i = Collections.binarySearch(rows, request);
+//                System.out.println(i);
+//                System.out.println(rows.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        long fnsh = System.currentTimeMillis();
-        long result = fnsh - strt;
-        System.out.println("Затрачено " + ((int) result) + " мс");
-        System.out.println("НАйдено строк" + lines);
 
-        scanner.close();
+        //-------------------------------------------------------------------------------------------------
+
+//        BufferedReader reader;
+//
+//        int lines = 0;
+//        try {
+//            reader = new BufferedReader(new FileReader("src\\main\\resources\\airports.dat"));
+//            String file;
+//            while ((file = reader.readLine()) != null) {
+//                String[] columns = file.split(",");
+//
+//                if (columns[column].indexOf(request) == 1){
+//                lines++;
+//                System.out.println( columns[column] + "[" + file + "]");
+//
+//            }
+//
+//
+////                Collections.sort(rows);
+////                int i = Collections.binarySearch(rows, request);
+////                System.out.println(i);
+////                System.out.println(rows.get(i));
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
 
+        //-------------------------------------------------------------------------------------------------
+
+
+//        File file = new File("src/main/resources/airports.dat");
+//
+//        Scanner scanner = new Scanner(file);
+//
+//        int lines = 0;
+//
+//        //ВЫбрать лучший способ поиска
+//        while (scanner.hasNextLine()){
+//            String line = scanner.nextLine();
+//            String[] columns = line.split(",");
+//
+//            //Исправить как он ищет
+//            if (columns[column].indexOf(request) == 1){
+//                lines++;
+//                System.out.println( columns[column] + "[" + line + "]");
+//
+//            }
+//
+//        }
+//
+//        scanner.close();
+
+        //--------------------------------------------------------------------------------------------
+
+        System.out.println(
+                "Количество найденых строк: " + lines + ", затрачено время на поиск: " +
+                        ((int) (System.currentTimeMillis() - strt)) + " мс"
+        );
 }}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,26 +4,21 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        //Column - выбранный столбец
         int column = Integer.valueOf(args[0]);
+        column --;
 
         while (true) {
 
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Selected column is " + column);
-            System.out.print("Enter request: ");
+            System.out.print("Введите строку: ");
             String request = scanner.nextLine();
-            //Request - что искать
 
             if (request.equals("!quit")) {
 
             } else if (request.length() != 0) {
-
                 FileSearch.searcher(request,column);
-
                 continue;
             } else {
-                System.out.println("Введите запрос");
                 continue;
             }
             break;
